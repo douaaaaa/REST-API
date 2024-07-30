37502f275c37ddfb4db4873d621cba8e27692ea9
Douâa Bouabdallah <bouabdallehdouaa@gmail.com>
2024-07-30 11:03:59 +0100

    create post a new user route

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const PORT = process.env.PORT;
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.use("/users", users);
 
 app.listen(PORT, (req, res) => {
